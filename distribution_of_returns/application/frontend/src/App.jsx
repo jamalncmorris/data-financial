0a36d89c635f8506343a07f561830f1215c5fe79
Jamal N.C Morris <jamalncmorris@gmail.com>
2024-01-19 00:01:53 +0000

    Trading view widget added
    Please see for examples of tickers message, needs to be added.

--- distribution_of_returns/application/frontend/src/App.jsx
+++ distribution_of_returns/application/frontend/src/App.jsx
@@ -4,6 +4,7 @@ import mainConstants from './constants/mainConstants.js'
 
 import Graph from './components/Graph.jsx'
 import Form from './components/Form.jsx'
+import TradingViewHeatMap from './components/TradingViewHeatMap.jsx'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <h1>{mainConstants.marketing.title}</h1>
       <Graph />
       <Form />
+      <div className='h-96'><TradingViewHeatMap/></div>
     </div>
   )
 }
